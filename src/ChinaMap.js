d601b7b4bd424d1d7f3371d8e5ea4ae6c993c4c2
linlin <1208215066@qq.com>
2023-09-13 14:17:57 +0800

    中国地图:显示了中国的各个省份

--- src/ChinaMap.js
+++ src/ChinaMap.js
@@ -1,50 +1,50 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
-import china from './china.json';
+import chinaGeoJson from 'china-geojson/src/geojson/china.json';
 
 const ChinaMap = () => {
   const svgRef = useRef(null);
-  const [selectedProvince, setSelectedProvince] = useState(null);
+  const [selectedProvince ] = useState(null);
 
   useEffect(() => {
-    const svgElement = d3.select(svgRef.current);
+    const svg = d3.select(svgRef.current);
 
-    // 创建地理投影
-    const projection = d3
-      .geoMercator()
+    // 设置地图的宽度和高度
+    const width = 800;
+    const height = 600;
+
+    // 创建一个地理投影
+    const projection = d3.geoMercator()
+    // 设置地图的中心位置
       .center([105, 38])
+    // 设置地图的缩放比例
       .scale(600)
-      .translate([500, 500]);
+    // 将地图移动到 SVG 的中心
+      .translate([width / 2, height / 2]);
 
-    // 创建路径生成器
-    const pathGenerator = d3.geoPath().projection(projection);
-
-    // 渲染地图路径
-    svgElement
-      .selectAll('path')
-      .data(china.features)
-      .enter()
-      .append('path')
-      .attr('d', pathGenerator)
-      .attr('stroke', '#333')
-      .attr('fill', 'lightblue')
-      .on('mouseover', (d) => {
-        d3.select(d.currentTarget).attr('fill', 'orange');
-      })
-      .on('mouseout', (d) => {
-        d3.select(d.currentTarget).attr('fill', 'lightblue');
-      })
-      .on('click', (d) => {
-        setSelectedProvince(d.properties.name);
-      });
-
-    // 缩放和平移功能
-    const zoomHandler = d3.zoom().on('zoom', () => {
-      svgElement.attr('transform', d.transform);
+    // 创建一个路径生成器
+    const path = d3.geoPath().projection(projection);
+    const features = JSON.parse(JSON.stringify(chinaGeoJson.features));
+    features.forEach((feature) => {
+      feature._entering = true;
     });
 
-    svgElement.call(zoomHandler);
-
+    svg.selectAll('*').remove();
+    svg.append('g')
+       .attr('transform', `translate(${width / 2},${height / 2})`)
+       .call(projection);
+    svg.selectAll('path')
+      .data(features)
+      .join('path')
+      .attr('d', path)
+      .attr('fill', (d) => (d._entering ? 'steelblue' : 'tomato'))
+      .transition()
+      .attr('fill-opacity', 0.8)
+      // 添加边框样式
+      // 边框颜色
+      .style('stroke', 'black')
+      // 边框宽度
+      .style('stroke-width', '1px');
   }, []);
 
   return (
