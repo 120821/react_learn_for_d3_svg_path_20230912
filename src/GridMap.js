90f6258fb6da2368be246fa9ded378ef575ab19e
linlin <1208215066@qq.com>
2023-10-07 15:40:25 +0800

    实现了某一年的数据展示为网格，比例还没调整，但是放大后是正常的

--- src/GridMap.js
+++ src/GridMap.js
@@ -26,6 +26,7 @@ const GridMap = () => {
       .attr("stroke", "blue")
       //  背景色: 整个地图
       .attr("fill", "none");
+
     // 创建网格
     const grid = d3.geoGraticule();
 
@@ -36,11 +37,54 @@ const GridMap = () => {
       .attr("stroke", "#ddd")
       .attr("fill", "none");
 
+    // 这里添加之前转换的 JSX 代码
+    d3.csv(chinaPopulationData).then((csv_data) => {
+
+      let areas = [];
+      let population = [];
+
+      // setup population
+      csv_data.forEach((line, index) => {
+        population.push(parseFloat(line['result_1990'])); // 修改这里，指定特定年份的数据
+      });
+
+      // 设置方块的颜色
+      let linear = d3.scaleLinear().domain(d3.extent(population)).range(['white', 'red']); // 修改这里，指定颜色范围
+      let tag_g = svg.append('g').attr('id', 'result_1990'); // 修改这里，指定特定年份的ID
+
+      // 开始画每个小方块。每一度一个小块
+      csv_data.forEach((line, index) => {
+        // 平时都是false, 仅仅在出问题的时候，使用后面的 index >=3
+        let is_debug = false; //index >= 3
+        if (is_debug) {
+          return;
+        }
+        let init_x = parseFloat(line.longitude);
+        let init_y = parseFloat(line.latitude);
+
+        let one_area = {
+          type: "Polygon",
+          coordinates: [
+            [
+              [init_x, init_y], [init_x, (init_y + 1)], [(init_x + 1), init_y + 1], [init_x + 1, init_y], [init_x, init_y]
+            ]
+          ]
+        };
+        //d3.select('#result_1990') // 修改这里，指定特定年份的ID
+        let column_name = "result_1990"; // 修改这里，指定特定年份的列名
+
+        let emission = line[column_name];
+        tag_g.append("path")
+          .attr("d", path(one_area))
+          .attr('fill', linear(parseFloat(line[column_name])))
+          .attr('opacity', 0.6);
+      });
+    });
+
   }, []);
 
   return (
     <svg ref={svgRef} width={500} height={500}></svg>
-
   );
 };
 
